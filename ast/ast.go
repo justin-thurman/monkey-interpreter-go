d2f5ab75cfba6551add2efbeacbaf91a380ec107
Justin Thurman <justin.thurman@bevy.com>
2024-03-19 16:55:41 -0400

    Prefix parser

--- ast/ast.go
+++ ast/ast.go
@@ -20,6 +20,23 @@ type Expression interface {
 	expressionNode()
 }
 
+type PrefixExpression struct {
+	Right    Expression
+	Token    token.Token // the prefix token, e.g., !
+	Operator string
+}
+
+func (pe *PrefixExpression) expressionNode()      {}
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+	return out.String()
+}
+
 type Program struct {
 	Statements []Statement
 }
